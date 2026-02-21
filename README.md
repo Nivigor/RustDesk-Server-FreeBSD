180c00a398bebc5b6d97051f12824807022d6460
Nivigor <79332259+Nivigor@users.noreply.github.com>
2026-02-21 15:04:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# RustDesk-Server-FreeBSD
+Install your own [open source RustDesk Server](https://github.com/rustdesk/rustdesk-server) on FreeBSD or XigmaNAS: 
+1. Ñreate a folder for server and go to it.
+2. Run ```fetch https://github.com/Nivigor/RustDesk-Server-FreeBSD/releases/latest/download/rustdeskserver.tgz```
+3. Run ```tar xzf rustdeskserver.tgz```
+4. Run ```./install.sh```
+For XigmaNAS embedded: Add install.sh to WebGUI: Advanced: Command Scripts as a PostInit command.
+-----
+[About RustDesk](https://rustdesk.com/)
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# filename:     install.sh
+# author:       Nivigor
+# date:         2026-02-20
+# purpose:      Install RustDesk Server
+
+DIR=$(dirname $(realpath $0))
+if [ ! -d /usr/local/etc/rc.d ]; then mkdir /usr/local/etc/rc.d; fi
+sed "s^%hbbdir%^${DIR}^" ${DIR}/hbbr.tmpl > /usr/local/etc/rc.d/hbbr
+sed "s^%hbbdir%^${DIR}^" ${DIR}/hbbs.tmpl > /usr/local/etc/rc.d/hbbs
+chmod +x /usr/local/etc/rc.d/hbbr /usr/local/etc/rc.d/hbbs
+
+if [ ! -d /usr/local/etc/newsyslog.conf.d ]; then mkdir /usr/local/etc/newsyslog.conf.d; fi
+sed "s^%hbbdir%^${DIR}^" ${DIR}/rustdesk.conf.tmpl > /usr/local/etc/newsyslog.conf.d/rustdesk.conf
+
+sysrc hbbr_enable="YES"
+sysrc hbbs_enable="YES"
+service hbbr start
+service hbbs start
